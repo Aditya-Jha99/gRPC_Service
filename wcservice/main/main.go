c98b053e643169999e50a8a149af37f842ff74f4
Aditya-Jha99 <99235372+Aditya-Jha99@users.noreply.github.com>
2022-03-28 10:22:41 +0530

    Update main.go

--- wcservice/main/main.go
+++ wcservice/main/main.go
@@ -1,10 +1,44 @@
 package main
 
 import (
-	"wcservice/api/post"
+	"fmt"
+	"wcservice/api"
+	"wcservice/configuration"
+	"wcservice/grpc"
+	"log"
+	"net"
 )
 
 func main() {
-	//fmt.Println("Aditya")
-	post.Request()
+
+	cfg, err := configuration.ReadConfigFromEnv()
+	if err != nil {
+		log.Fatalf("failed to read env: %v", err)
+	}
+
+	var errs chan error
+
+	r := api.Setup()
+
+	go func() {
+		errs <- r.Run(fmt.Sprintf(":%d", cfg.RestPort))
+	}()
+
+	lis, err := net.Listen("",fmt.Sprintf(":%d", cfg.GRPCPort))
+	if err != nil {
+		log.Fatalf("Failed to Listen: %v", err)
+	}
+
+	s := grpc.Setup()
+
+	go func() {
+		fmt.Printf("\ngRPC server Listening at %v\n", lis.Addr())
+		errs <- s.Serve(lis)
+	}()
+
+	if err = <-errs; err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+
+	s.GracefulStop()
 }
